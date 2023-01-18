adf3e8a0f03e1cddaee28325a27a78fd44b811b7
Julien Defforge <julien.def@outlook.fr>
2023-01-18 19:39:09 +0100

    ListViewModel & Details

--- View/Logbook/Component/LogBookList.xaml.cs
+++ View/Logbook/Component/LogBookList.xaml.cs
@@ -1,9 +1,22 @@
+using th_pl.Model;
+using th_pl.ViewModel;
+
 namespace th_pl.View.Logbook.Component;
 
 public partial class LogBookList : ContentView
 {
+	private LogBookListViewModel ViewModel;
+
 	public LogBookList()
 	{
 		InitializeComponent();
+		ViewModel = BindingContext as LogBookListViewModel;
+	}
+
+	private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+	{
+		var diveSelected = e.Item as DiveModel;
+		ViewModel.SetDiveModel(diveSelected);
+		await Navigation.PushModalAsync(new LogBookDiveDetails());
 	}
 }
--- ViewModel/LogBookListViewModel.cs
+++ ViewModel/LogBookListViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using th_pl.Model;
+using th_pl.Repository;
 
 namespace th_pl.ViewModel
 {
-    internal class LogBookListViewModel
+    // ViewModel qui sera utilisé pour afficher la liste des DiveModel
+    // et le detail d'un DiveModel
+    public class LogBookListViewModel : ObservableObject
     {
+        public ObservableCollection<DiveModel> Dives { get; set; }
+
+        private DiveRepository Repository { get; set; } = new();
+
+        public DiveModel CurrentDive { get; set; } = new();
+
+        public LogBookListViewModel()
+        {
+            var list = Repository.GetList().GetAwaiter().GetResult();
+			Dives = new ObservableCollection<DiveModel>(list);
+        }
+
+        public void SetDiveModel(DiveModel diveModel)
+        {
+            CurrentDive = diveModel;
+        }
     }
 }
